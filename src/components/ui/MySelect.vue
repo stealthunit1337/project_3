7399fdc962af9d8a5fa10adc3e5983688a290dc4
stealthunit1337 <stealthunit123@gmail.com>
2022-08-02 16:15:44 +0300

    Kappa

--- src/components/ui/MySelect.vue
+++ src/components/ui/MySelect.vue
@@ -2,20 +2,44 @@
   <slot>
     <select name="" id="">
       <option disabled selected>Choose an option</option>
-      <options v-for="option in options" value=""></options>
+      <option v-for="item in items" @click="someMethod(item.id)">{{ item.name }}</option>
     </select>
   </slot>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed, ref } from 'vue';
 
-export default defineComponent({
-  name: 'MySelect',
-  props: {
-    options: Array,
-  },
-});
+export default
+  defineComponent({
+    name: 'MySelect',
+    props: {
+      items: {
+        type: [Object]
+      },
+      id: {
+        type: Number
+      },
+      isVisible: {
+        type: Number
+      }
+    },
+    setup(props, { emit }) {
+
+      const currentValue = computed({
+        get: () => props.id,
+        set: (e) => emit("update:isVisible", e)
+      });
+
+      const someMethod = (id: Number) => {
+        emit("update:isVisible", id)
+        console.log('123')
+      }
+      return {
+        someMethod
+      }
+    }
+  });
 </script>
 
 <style lang="scss" scoped>
--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
+    <div v-if="isVisible == 1">qwe</div>
+    <div v-if="isVisible == 2">zxc</div>
     <p>123213</p>
-    <my-select></my-select>
+    <my-select v-model:isVisible="isVisible" :items="items"></my-select>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import MySelect from '@/components/ui/MySelect.vue';
 
 export default defineComponent({
@@ -14,7 +16,19 @@ export default defineComponent({
   components: {
     MySelect,
   },
+  setup() {
+    const items = [
+      { name: "kappa", id: 1 },
+      { name: "keepo", id: 2 },
+    ]
+    const isVisible = ref()
+    return {
+      items,
+      isVisible
+    }
+  }
 });
+
 </script>
 
 <style lang="scss" scoped >
